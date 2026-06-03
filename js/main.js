eccc33cff898559b745c5b8cead11e2a7fd04a37
antonin-gg <guyette.anto@gmail.com>
2026-06-03 10:48:40 +0200

    Add updates

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ document.querySelector('[data-lang="' + uiLang + '"]').classList.add("active-lan
  * Dark mode uses Stadia for a more polished look, with CARTO's dark_all as a
  * fallback if Stadia tile requests fail.
  *
- * Satellite uses MapTiler imagery with Esri as a fallback.
+ * Satellite uses Esri imagery
  */
 
 const mapLayerLight = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
@@ -81,20 +81,9 @@ const mapLayerDark = L.tileLayer(
         e.coords.z + '/' + e.coords.x + '/' + e.coords.y + '.png';
 });
 
-const satelliteLayer = L.tileLayer(
-    'https://api.maptiler.com/maps/019e8c9b-1039-7975-8634-e7145694a9be/{z}/{x}/{y}.jpg?key=' + MAPTILER_API_KEY,
-    {
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        maxZoom: 19,
-        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a>',
-        crossOrigin: true,
-        errorTileUrl: ''
-    }
-).on('tileerror', function (e) {
-    e.tile.src = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/' +
-        e.coords.z + '/' + e.coords.y + '/' + e.coords.x;
+const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 18,
+    attribution: '© Esri'
 });
 
 const map = L.map('map').setView([0, 0], 2);
